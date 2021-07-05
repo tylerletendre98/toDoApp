a0b863dd2a81df73c565493de652b90e14ac9d96
T U <tylerletendre98@gmail.com>
2021-07-05 15:23:13 -0400

    added in a header

---
+++ src/components/header/header.jsx
@@ -0,0 +1,21 @@
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="container">
+        <div class="row">
+          <div class="col"></div>
+          <div class="col-6">
+            <div className="app-title">
+              <h1>To Do App</h1>{" "}
+            </div>
+          </div>
+          <div class="col"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
