1409542a9342113c54ddef17b66212f5b4e11bde
T U <tylerletendre98@gmail.com>
2021-07-05 16:09:56 -0400

    added in a form to add a todo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import "./App.css";
 import Header from "./components/header/header";
+import ToDoForm from "./components/toDoForm/ToDoForm";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <dir>
-        <Header />
-      </dir>
-    </div>
-  );
+class App extends Component {
+  state = {
+    toDos: [],
+  };
+  addToDo = (todo) => {
+    this.state.toDos.push(todo);
+  };
+  render() {
+    return (
+      <div className="App">
+        <dir>
+          <Header />
+          <ToDoForm />
+        </dir>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/toDoForm/ToDoForm.jsx
@@ -0,0 +1,73 @@
+import { Component } from "react";
+
+class ToDoForm extends Component {
+  state = {
+    title: "",
+    description: "",
+    date: "",
+  };
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+  render() {
+    return (
+      <div>
+        <div class="row">
+          <div class="col"></div>
+          <div class="col-5">
+            <form action="" onSubmit={this.handleSubmit}>
+              <div className="todo-form">
+                <div class="mb-3">
+                  <label for="exampleFormControlInput1" class="form-label">
+                    Title
+                  </label>
+                  <input
+                    name="title"
+                    type="text"
+                    class="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="Name of what needs to get done"
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div class="mb-3">
+                  <label for="exampleFormControlTextarea1" class="form-label">
+                    What needs to get done
+                  </label>
+                  <textarea
+                    name="description"
+                    class="form-control"
+                    id="exampleFormControlTextarea1"
+                    rows="3"
+                    placeholder="Something that needs to get done"
+                    onChange={this.handleChange}
+                  ></textarea>
+                  <div class="mb-3">
+                    <label htmlFor="" className="form-label">
+                      What date needs to be done by:
+                    </label>
+                    <input
+                      type="date"
+                      name="date"
+                      id=""
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                  <div>
+                    <input type="submit" name="" id="" />
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="col"></div>
+        </div>
+      </div>
+    );
+  }
+}
+export default ToDoForm;
