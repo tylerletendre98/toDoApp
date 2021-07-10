b569e6e17c3daa1c468f8840ba5cb8c1a3c68d95
T U <tylerletendre98@gmail.com>
2021-07-10 12:28:07 -0400

    conditionally renders if there are no todos it displays showing there are no todos if you add a todo it changes the to the todos in the todo array

--- src/components/displayTodos/DisplayTodos.jsx
+++ src/components/displayTodos/DisplayTodos.jsx
@@ -1,26 +1,34 @@
 const DisplayTodos = (props) => {
-  //   console.log(props.toDos);
-  return (
-    <div>
+  console.log(props.toDos);
+  if (props.toDos.length === 0) {
+    return (
       <div>
-        {props.toDos.map((todo) => {
-          return (
-            <div>
-              <div>
-                <h4>{todo.title}</h4>
-              </div>
-              <div>
-                <p>{todo.description}</p>
-              </div>
-              <div>
-                <p>{todo.date}</p>
-              </div>
-            </div>
-          );
-        })}
+        <h3>You have nothing to do!</h3>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div>
+        <div>
+          {props.toDos.map((todo) => {
+            return (
+              <div>
+                <div>
+                  <h4>{todo.title}</h4>
+                </div>
+                <div>
+                  <p>{todo.description}</p>
+                </div>
+                <div>
+                  <p>{todo.date}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 };
 
 export default DisplayTodos;
